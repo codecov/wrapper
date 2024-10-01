969f50b635decc6b4f3ddc8149d44ea9a74753ae
Tom Hu <thomas.hu@sentry.io>
2024-10-01 17:20:30 -0500

    fix: add -Z

--- upload.sh
+++ upload.sh
@@ -49,7 +49,7 @@ echo "./\"$codecov_filename\" ${CODECOV_CLI_ARGS} create-report -t <redacted>"
 #upload reports
 # alpine doesn't allow for indirect expansion
 
-echo "./${codecov_filename} ${CODECOV_CLI_ARGS} do-upload -t <redacted> -n \"${CODECOV_UPLOAD_NAME}\" ${FLAGS} ${CODECOV_UPLOAD_ARGS} ${@}"
+echo "./${codecov_filename} ${CODECOV_CLI_ARGS} do-upload -Z -t <redacted> -n \"${CODECOV_UPLOAD_NAME}\" ${FLAGS} ${CODECOV_UPLOAD_ARGS} ${@}"
 
 ./$codecov_filename \
   ${CODECOV_CLI_ARGS} \
@@ -59,4 +59,3 @@ echo "./${codecov_filename} ${CODECOV_CLI_ARGS} do-upload -t <redacted> -n \"${C
   ${FLAGS} \
   ${CODECOV_UPLOAD_ARGS} \
   ${@}
-echo $?
