23fb3537f20127d868d83a284c6799e74ba3148c
Zxilly <31370133+Zxilly@users.noreply.github.com>
2024-11-19 15:40:16 +0000

    feat: update upload command

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
+. ./version.sh
+. ./set_defaults.sh
+
 if [ -n "$CC_USE_PYTHON" ]; then
   echo "CC_USE_PYTHON is set, skipping download and validation steps."
 else
   . ./download.sh
   . ./validate.sh
 fi
-
-. ./version.sh
-. ./set_defaults.sh
 
 . ./set_cli_args.sh
 . ./set_create_commit_args.sh
