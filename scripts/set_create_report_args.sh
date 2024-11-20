a92bfaa01d88e44d0c16ddd2d5cd1d09df29d404
Tom Hu <thomas.hu@sentry.io>
2024-11-19 19:17:23 -0500

    fix: update var usage to split properly when run

--- scripts/set_create_report_args.sh
+++ scripts/set_create_report_args.sh
@@ -2,9 +2,9 @@
 
 codecov_create_report_args=()
 
-codecov_cr_args+=( $(write_existing_args CODECOV_CODE) )
+codecov_cr_args+=( $(k_arg CODE) $(v_arg CODE))
 codecov_cr_args+=( $(write_truthy_args CODECOV_FAIL_ON_ERROR) )
-codecov_cr_args+=( $(write_existing_args CODECOV_GIT_SERVICE) )
-codecov_cr_args+=( $(write_existing_args CODECOV_PR) )
-codecov_cr_args+=( $(write_existing_args CODECOV_SHA) )
-codecov_cr_args+=( $(write_existing_args CODECOV_SLUG) )
+codecov_cr_args+=( $(k_arg GIT_SERVICE) $(v_arg GIT_SERVICE))
+codecov_cr_args+=( $(k_arg PR) $(v_arg PR))
+codecov_cr_args+=( $(k_arg SHA) $(v_arg SHA))
+codecov_cr_args+=( $(k_arg SLUG) $(v_arg SLUG))
